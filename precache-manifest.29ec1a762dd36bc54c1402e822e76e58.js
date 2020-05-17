6786639cc8db0f0e3b7b0b1d378d127ead90aa13
Philippe Potin Flen <papotin@uc.cl>
2020-05-16 20:31:24 -0400

    Updates

---
+++ precache-manifest.29ec1a762dd36bc54c1402e822e76e58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a0f0ae49dd5ee42e3a020957ba30d1",
+    "url": "/IIC3103-Tarea3-ppotin/index.html"
+  },
+  {
+    "revision": "dc66e5a7f99160018e0c",
+    "url": "/IIC3103-Tarea3-ppotin/static/css/main.a7a99e7a.chunk.css"
+  },
+  {
+    "revision": "d9eda29d3f4024ba43c9",
+    "url": "/IIC3103-Tarea3-ppotin/static/js/2.925ca79b.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/IIC3103-Tarea3-ppotin/static/js/2.925ca79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc66e5a7f99160018e0c",
+    "url": "/IIC3103-Tarea3-ppotin/static/js/main.566592f3.chunk.js"
+  },
+  {
+    "revision": "9823458786ade8027f50",
+    "url": "/IIC3103-Tarea3-ppotin/static/js/runtime-main.91745410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/IIC3103-Tarea3-ppotin/static/media/logo.5d5d9eef.svg"
+  }
+]);
